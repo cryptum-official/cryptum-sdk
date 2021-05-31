5db41d65e24bf07d3de26def439067df3919e759
Yury Alencar <yuryalencar19@gmail.com>
2021-05-31 11:50:04 -0300

    feat(test): added description to test file

--- src/features/user/controller/index.js
+++ src/features/user/controller/index.js
@@ -1,9 +1,10 @@
+// THIS FILE CONTAINS A UNIQUE PURPOSE TEST IMPLEMENTATION.
+// WE NEED ADD TEST FILES YET
 const UseCases = require('../use-cases')
 const { handleRequestError } = require('../../../services')
 
 const Adapter = require('../adapter')
 const Interface = require('./interface')
-
 class Controller extends Interface {
   async auth(credentials) {
     try {
