d9f626308c73b1925f216bdd0b3c75b095902e3a
Yury Alencar <yuryalencar19@gmail.com>
2021-06-09 11:13:29 -0300

    feat(webhooks): added webhooks controller tests

--- tests/webhooks/controller.test.js
+++ tests/webhooks/controller.test.js
@@ -1,9 +1,55 @@
 const assert = require('assert')
+const nock = require('nock')
+
 const WebhookCryptum = require('../../src/features/webhooks/entity')
-const WebhookCryptumUseCase = require('../../src/features/webhooks/use-cases')
+const WebhookCryptumController = require('../../src/features/webhooks/controller')
 const WebhookCryptumInterface = require('../../src/features/webhooks/controller/interface')
 
 describe.only('Test Suite of the Webhook (Controller)', function () {
+  this.beforeAll(() => {
+    const postResponse = {
+      id: '143c07af-cc73-4d46-9e0a-8d96624a082e',
+    }
+
+    const getResponse = [
+      {
+        id: '68c2e098-12e0-435e-aabe-f2ed5a581672',
+        event: 'tx-confirmation',
+        url: 'https://site.com',
+        address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
+        confirmations: 6,
+      },
+    ]
+
+    nock('https://api-dev.cryptum.io', {
+      reqheaders: { 'x-api-key': 'apikeyexamplecryptum' },
+    })
+      .post('/webhook/BTC', {
+        asset: 'BTC',
+        url: 'https://site.com',
+        event: 'tx-confirmation',
+        address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
+        confirmations: '6',
+        protocol: 'BITCOIN',
+      })
+      .reply(200, postResponse)
+
+    const params = new URLSearchParams({ protocol: 'BITCOIN' })
+    nock('https://api-dev.cryptum.io', {
+      reqheaders: { 'x-api-key': 'apikeyexamplecryptum' },
+    })
+      .get('/webhook/BTC')
+      .query(params)
+      .reply(200, getResponse)
+
+    nock('https://api-dev.cryptum.io', {
+      reqheaders: { 'x-api-key': 'apikeyexamplecryptum' },
+    })
+      .delete('/webhook/BTC/143c07af-cc73-4d46-9e0a-8d96624a082e')
+      .query(params)
+      .reply(200)
+  })
+
   it('Check if an Not Implemented Exception in interface (Interface test) : createWebhook', async () => {
     const expectedResult = 'Method not implemented'
 
@@ -52,326 +98,62 @@ describe.only('Test Suite of the Webhook (Controller)', function () {
     }
   })
 
-  // it('Check if can mount an Webhook Cryptum to create in Cryptum API (With valid data) : mountWebhookToCreate', async () => {
-  //   const data = {
-  //     asset: 'BTC',
-  //     event: 'tx-confirmation',
-  //     url: 'https://site.com',
-  //     address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
-  //     confirmations: 6,
-  //     protocol: 'BITCOIN',
-  //   }
+  it('Check create an webhook with WebhookCryptum valid : createWebhook', async () => {
+    const data = {
+      asset: 'BTC',
+      url: 'https://site.com',
+      event: 'tx-confirmation',
+      address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
+      confirmations: '6',
+      protocol: 'BITCOIN',
+    }
 
-  //   const expectedResult = new WebhookCryptum(data)
+    const webhook = new WebhookCryptum(data)
+    const expectedResult = new WebhookCryptum({
+      ...webhook,
+      id: '143c07af-cc73-4d46-9e0a-8d96624a082e',
+    })
 
-  //   const result = WebhookCryptumUseCase.mountWebhookToCreate(data)
-  //   assert.deepStrictEqual(result, expectedResult)
-  // })
+    const controller = new WebhookCryptumController({
+      enviroment: 'development',
+      apiKey: 'apikeyexamplecryptum',
+    })
+    const result = await controller.createWebhook(webhook)
+    assert.deepStrictEqual(result, expectedResult)
+  })
 
-  // it('Check if can mount an Webhook Cryptum to create in Cryptum API (Without asset) : mountWebhookToCreate', async () => {
-  //   const expectedResult = 'Not can mount WebhookCryptum entity'
+  it('Check destroy an webhook with WebhookCryptum valid : destroyWebhook', async () => {
+    const expectedResult = ''
+    const data = {
+      webhookId: '143c07af-cc73-4d46-9e0a-8d96624a082e',
+      asset: 'BTC',
+      protocol: 'BITCOIN',
+    }
 
-  //   const data = {
-  //     asset: 'BTC',
-  //     url: 'https://site.com',
-  //     event: 'tx-confirmation',
-  //     address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
-  //     confirmations: '6',
-  //     protocol: 'BITCOIN',
-  //   }
+    const controller = new WebhookCryptumController({
+      enviroment: 'development',
+      apiKey: 'apikeyexamplecryptum',
+    })
+    const result = await controller.destroyWebhook(data)
+    assert.deepStrictEqual(result, expectedResult)
+  })
 
-  //   try {
-  //     WebhookCryptumUseCase.mountWebhookToCreate(data)
-  //   } catch (error) {
-  //     assert.deepStrictEqual(error.message, expectedResult)
-  //   }
-  // })
+  it('Check get webhooks : getWebhooks', async () => {
+    const expectedResult = [
+      new WebhookCryptum({
+        id: '68c2e098-12e0-435e-aabe-f2ed5a581672',
+        event: 'tx-confirmation',
+        url: 'https://site.com',
+        address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
+        confirmations: 6,
+      }),
+    ]
 
-  // it('Check if can mount an Webhook Cryptum to create in Cryptum API (Without url) : mountWebhookToCreate', async () => {
-  //   const expectedResult = 'Not can mount WebhookCryptum entity'
-
-  //   const data = {
-  //     asset: 'BTC',
-  //     event: 'tx-confirmation',
-  //     address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
-  //     confirmations: '6',
-  //     protocol: 'BITCOIN',
-  //   }
-
-  //   try {
-  //     WebhookCryptumUseCase.mountWebhookToCreate(data)
-  //   } catch (error) {
-  //     assert.deepStrictEqual(error.message, expectedResult)
-  //   }
-  // })
-
-  // it('Check if can mount an Webhook Cryptum to create in Cryptum API (Without event) : mountWebhookToCreate', async () => {
-  //   const expectedResult = 'Not can mount WebhookCryptum entity'
-
-  //   const data = {
-  //     asset: 'BTC',
-  //     url: 'https://site.com',
-  //     address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
-  //     confirmations: '6',
-  //     protocol: 'BITCOIN',
-  //   }
-
-  //   try {
-  //     WebhookCryptumUseCase.mountWebhookToCreate(data)
-  //   } catch (error) {
-  //     assert.deepStrictEqual(error.message, expectedResult)
-  //   }
-  // })
-
-  // it('Check if can mount an Webhook Cryptum to create in Cryptum API (Without address) : mountWebhookToCreate', async () => {
-  //   const expectedResult = 'Not can mount WebhookCryptum entity'
-
-  //   const data = {
-  //     asset: 'BTC',
-  //     url: 'https://site.com',
-  //     event: 'tx-confirmation',
-  //     confirmations: '6',
-  //     protocol: 'BITCOIN',
-  //   }
-
-  //   try {
-  //     WebhookCryptumUseCase.mountWebhookToCreate(data)
-  //   } catch (error) {
-  //     assert.deepStrictEqual(error.message, expectedResult)
-  //   }
-  // })
-
-  // it('Check if can mount an Webhook Cryptum to create in Cryptum API (Without confirmations) : mountWebhookToCreate', async () => {
-  //   const expectedResult = 'Not can mount WebhookCryptum entity'
-
-  //   const data = {
-  //     asset: 'BTC',
-  //     url: 'https://site.com',
-  //     event: 'tx-confirmation',
-  //     address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
-  //     protocol: 'BITCOIN',
-  //   }
-
-  //   try {
-  //     WebhookCryptumUseCase.mountWebhookToCreate(data)
-  //   } catch (error) {
-  //     assert.deepStrictEqual(error.message, expectedResult)
-  //   }
-  // })
-
-  // it('Check if can mount an Webhook Cryptum to create in Cryptum API (Without protocol) : mountWebhookToCreate', async () => {
-  //   const expectedResult = 'Not can mount WebhookCryptum entity'
-
-  //   const data = {
-  //     asset: 'BTC',
-  //     url: 'https://site.com',
-  //     event: 'tx-confirmation',
-  //     address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
-  //     confirmations: '6',
-  //   }
-
-  //   try {
-  //     WebhookCryptumUseCase.mountWebhookToCreate(data)
-  //   } catch (error) {
-  //     assert.deepStrictEqual(error.message, expectedResult)
-  //   }
-  // })
-
-  // it('Check if can mount an Webhook Cryptum to create in Cryptum API (With invalid protocol) : mountWebhookToCreate', async () => {
-  //   const expectedResult = 'Not can mount WebhookCryptum entity'
-
-  //   const data = {
-  //     asset: 'BTC',
-  //     url: 'https://site.com',
-  //     event: 'tx-confirmation',
-  //     address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
-  //     confirmations: '6',
-  //     protocol: 'BITCOINS',
-  //   }
-
-  //   try {
-  //     WebhookCryptumUseCase.mountWebhookToCreate(data)
-  //   } catch (error) {
-  //     assert.deepStrictEqual(error.message, expectedResult)
-  //   }
-  // })
-
-  // it('Check if can mount an Webhook Cryptum to create in Cryptum API (With invalid event) : mountWebhookToCreate', async () => {
-  //   const expectedResult = 'Not can mount WebhookCryptum entity'
-
-  //   const data = {
-  //     asset: 'BTC',
-  //     url: 'https://site.com',
-  //     event: 'tx-confirmations',
-  //     address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
-  //     confirmations: '6',
-  //     protocol: 'BITCOIN',
-  //   }
-
-  //   try {
-  //     WebhookCryptumUseCase.mountWebhookToCreate(data)
-  //   } catch (error) {
-  //     assert.deepStrictEqual(error.message, expectedResult)
-  //   }
-  // })
-
-  // it('Check if can mount an Webhook Cryptum to create in Cryptum API (With invalid event - Using WebhookCryptumObject) : mountWebhookToCreate', async () => {
-  //   const expectedResult = 'Not can mount WebhookCryptum entity'
-
-  //   const data = {
-  //     asset: 'BTC',
-  //     url: 'https://site.com',
-  //     event: 'tx-confirmations',
-  //     address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
-  //     confirmations: '6',
-  //     protocol: 'BITCOIN',
-  //   }
-
-  //   try {
-  //     const webhook = new WebhookCryptum(data)
-  //     WebhookCryptumUseCase.mountWebhookToCreate(webhook)
-  //   } catch (error) {
-  //     assert.deepStrictEqual(error.message, expectedResult)
-  //   }
-  // })
-
-  // it('Check if can mount an Webhook Cryptum to create in Cryptum API (With invalid protocol - Using WebhookCryptumObject) : mountWebhookToCreate', async () => {
-  //   const expectedResult = 'Not can mount WebhookCryptum entity'
-
-  //   const data = {
-  //     asset: 'BTC',
-  //     url: 'https://site.com',
-  //     event: 'tx-confirmation',
-  //     address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
-  //     confirmations: '6',
-  //     protocol: 'BITCOINS',
-  //   }
-
-  //   try {
-  //     const webhook = new WebhookCryptum(data)
-  //     WebhookCryptumUseCase.mountWebhookToCreate(webhook)
-  //   } catch (error) {
-  //     assert.deepStrictEqual(error.message, expectedResult)
-  //   }
-  // })
-
-  // it('Check if can mount an Webhook Cryptum to create in Cryptum API (With valid data) : mountWebhook', async () => {
-  //   const data = {
-  //     id: 'ed34b1e3-9689-4976-8277-c8e97768beaa',
-  //     event: 'tx-confirmation',
-  //     url: 'https://site.com',
-  //     address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
-  //     confirmations: 6,
-  //   }
-
-  //   const expectedResult = new WebhookCryptum(data)
-
-  //   const result = WebhookCryptumUseCase.mountWebhook(data)
-  //   assert.deepStrictEqual(result, expectedResult)
-  // })
-
-  // it('Check if can mount an Webhook Cryptum to create in Cryptum API (Without url) : mountWebhook', async () => {
-  //   const expectedResult = 'Not can mount WebhookCryptum entity'
-
-  //   const data = {
-  //     id: 'ed34b1e3-9689-4976-8277-c8e97768beaa',
-  //     event: 'tx-confirmation',
-  //     address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
-  //     confirmations: 6,
-  //   }
-
-  //   try {
-  //     WebhookCryptumUseCase.mountWebhook(data)
-  //   } catch (error) {
-  //     assert.deepStrictEqual(error.message, expectedResult)
-  //   }
-  // })
-
-  // it('Check if can mount an Webhook Cryptum to create in Cryptum API (Without event) : mountWebhook', async () => {
-  //   const expectedResult = 'Not can mount WebhookCryptum entity'
-
-  //   const data = {
-  //     id: 'ed34b1e3-9689-4976-8277-c8e97768beaa',
-  //     url: 'https://site.com',
-  //     address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
-  //     confirmations: 6,
-  //   }
-
-  //   try {
-  //     WebhookCryptumUseCase.mountWebhook(data)
-  //   } catch (error) {
-  //     assert.deepStrictEqual(error.message, expectedResult)
-  //   }
-  // })
-
-  // it('Check if can mount an Webhook Cryptum to create in Cryptum API (Without address) : mountWebhook', async () => {
-  //   const expectedResult = 'Not can mount WebhookCryptum entity'
-
-  //   const data = {
-  //     id: 'ed34b1e3-9689-4976-8277-c8e97768beaa',
-  //     event: 'tx-confirmation',
-  //     url: 'https://site.com',
-  //     confirmations: 6,
-  //   }
-
-  //   try {
-  //     WebhookCryptumUseCase.mountWebhook(data)
-  //   } catch (error) {
-  //     assert.deepStrictEqual(error.message, expectedResult)
-  //   }
-  // })
-
-  // it('Check if can mount an Webhook Cryptum to create in Cryptum API (Without confirmations) : mountWebhook', async () => {
-  //   const expectedResult = 'Not can mount WebhookCryptum entity'
-
-  //   const data = {
-  //     id: 'ed34b1e3-9689-4976-8277-c8e97768beaa',
-  //     event: 'tx-confirmation',
-  //     url: 'https://site.com',
-  //     address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
-  //   }
-
-  //   try {
-  //     WebhookCryptumUseCase.mountWebhook(data)
-  //   } catch (error) {
-  //     assert.deepStrictEqual(error.message, expectedResult)
-  //   }
-  // })
-
-  // it('Check if can mount an Webhook Cryptum to create in Cryptum API (With invalid event) : mountWebhook', async () => {
-  //   const expectedResult = 'Not can mount WebhookCryptum entity'
-
-  //   const data = {
-  //     id: 'ed34b1e3-9689-4976-8277-c8e97768beaa',
-  //     event: 'tx-confirmations',
-  //     url: 'https://site.com',
-  //     address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
-  //     confirmations: 6,
-  //   }
-
-  //   try {
-  //     WebhookCryptumUseCase.mountWebhook(data)
-  //   } catch (error) {
-  //     assert.deepStrictEqual(error.message, expectedResult)
-  //   }
-  // })
-
-  // it('Check if can mount an Webhook Cryptum to create in Cryptum API (With invalid event - Using WebhookCryptumObject) : mountWebhook', async () => {
-  //   const expectedResult = 'Not can mount WebhookCryptum entity'
-
-  //   const data = {
-  //     id: 'ed34b1e3-9689-4976-8277-c8e97768beaa',
-  //     event: 'tx-confirmations',
-  //     url: 'https://site.com',
-  //     address: '0x0c99adab65a55df5faf53ab923f43d9eb9368772',
-  //     confirmations: 6,
-  //   }
-
-  //   try {
-  //     const webhook = new WebhookCryptum(data)
-  //     WebhookCryptumUseCase.mountWebhook(webhook)
-  //   } catch (error) {
-  //     assert.deepStrictEqual(error.message, expectedResult)
-  //   }
-  // })
+    const controller = new WebhookCryptumController({
+      enviroment: 'development',
+      apiKey: 'apikeyexamplecryptum',
+    })
+    const result = await controller.getWebhooks('BTC', 'BITCOIN')
+    assert.deepStrictEqual(result, expectedResult)
+  })
 })
