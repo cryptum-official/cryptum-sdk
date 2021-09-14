73e6b7e94243449c3d83549f37f25a9f6deb2c3b
Victor Municelli Dario <victor@blockforce.in>
2021-09-13 21:41:27 -0300

    webhook api fixes

--- src/features/webhooks/adapter/interface.js
+++
@@ -1,3 +0,0 @@
-class Interface {}
-
-module.exports = Interface
--- tests/prices/mocks.js
+++ tests/prices/mocks.js
@@ -4,15 +4,7 @@ module.exports = {
   pricesMock: {
     BRL: faker.datatype.number(),
     USD: faker.datatype.number(),
-    JPY: faker.datatype.number(),
     EUR: faker.datatype.number(),
-    GBP: faker.datatype.number(),
-    AUD: faker.datatype.number(),
-    CAD: faker.datatype.number(),
-    CHF: faker.datatype.number(),
-    CNH: faker.datatype.number(),
-    SEK: faker.datatype.number(),
-    NZD: faker.datatype.number(),
     ETH: faker.datatype.number(),
     XLM: faker.datatype.number(),
     MDA: faker.datatype.number(),
@@ -20,5 +12,6 @@ module.exports = {
     XRP: faker.datatype.number(),
     CELO: faker.datatype.number(),
     BTC: faker.datatype.number(),
+    HTR: faker.datatype.number(),
   }
 };
