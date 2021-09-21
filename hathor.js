67b8a1901874a9866242329ad06b2ababef7deab
Vagner Ribeiro <vagner@blockforce.in>
2021-09-21 04:45:26 -0300

    Send signed transactions

--- hathor.js
+++ hathor.js
@@ -7,20 +7,20 @@ const hathor = new CryptumSDK({
 });
 
 const walletController = hathor.getWalletController()
+const mnemonic = "drive brain exile silly release bacon trial song lemon steel salt resemble train latin fitness sea waste member measure regret hold kind soldier remember";
 
-walletController.generateWalletFromPrivateKey({
-  privateKey: '19a5965a0f6a8b95a74907881f77020de5f53c7084ffb3d0f7ec201056562a15',
-  protocol: 'HATHOR',
-}).then((wallet) => {
+walletController.generateHathorWallet(mnemonic, true).then((wallet) => {
+  
   const txController = hathor.getTransactionController()
-
+  
   txController.createHathorTransferTransaction({
     wallet,
-    outputs: [{ address: 'WfJqB5SNHnkwXCCGLMBVPcwuVr94hq1oKH', amount: 5 }],
+    outputs: [{ address: 'WXngE15Yynt1nEut4EG2C2C5evoxp4gAGm', amount: 1 }],
     testnet: true
   }).then((transaction) => {
-    console.log(transaction);
-    // const { hash } =  txController.sendTransaction(transaction)
-    console.log(transaction)
+    const { hash } =  txController.sendTransaction(transaction)
+    console.log(hash)
+  }).catch((e) => {
+    console.log(e)
   })
 })
--- src/services/blockchain/hathor.js
+++ src/services/blockchain/hathor.js
@@ -1,58 +1,50 @@
 const hathorLib = require("@hathor/wallet-lib");
-const transaction = require("@hathor/wallet-lib/lib/utils/transaction");
-const { default: BigNumber } = require("bignumber.js");
 const { storageFactory } = require('storage-factory');
-
 
 hathorLib.storage.setStore(storageFactory(() => 'localStorage'));
 
-module.exports.buildHathorTransferTransaction = async function ({ wallet, inputs, outputs }) {
+module.exports.buildHathorTransferTransaction = async function ({ wallet, outputs }) {
 
     const network = new hathorLib.Network((wallet.testnet) ? 'testnet' : 'mainnet');
+    const walletService = new hathorLib.HathorWalletServiceWallet(wallet.mnemonic, network);
+    await walletService.start()
 
-    const utxos = transaction.default
-    const selectedUtxos = utxos.selectUtxos(inputs, 5);
-
-    let totalAmount = 0
-
-    for (let i = 0; i < outputs.length; i++) {
-        totalAmount = totalAmount + outputs[i].amount
-    }
-
-    let txData = {inputs: [],outputs: []}
-
-    for (let i = 0; i < selectedUtxos.utxos.length; i++) {
-        txData.inputs[i] = new hathorLib.Input(selectedUtxos.utxos[i].txHash, selectedUtxos.utxos[i].index);
-    }
-
-
-    outputs.push({
-        address: 'Wj2naTrfbE7ffBLK9pjR1Eb8TwbBcBE5Ya',
-        amount: selectedUtxos.changeAmount
-    });
+    let txData = { outputs: [] }
 
     for (let j = 0; j < outputs.length; j++) {
+
         let address = new hathorLib.Address(outputs[j].address)
-        let P2PKH  = new hathorLib.P2PKH(address)
+        let P2PKH = new hathorLib.P2PKH(address)
         let script = P2PKH.createScript();
-        txData.outputs[j] = new hathorLib.Output(outputs[j].amount,script)
+
+        txData.outputs[j] = {
+            'value': outputs[j].amount,
+            'address': outputs[j].address,
+            'script': script,
+            'tokenData':  0,
+            'token': '00',
+        }
     }
 
-    let _tx = new hathorLib.Transaction(txData.inputs, txData.outputs)
+    const allAddress = await walletService.getAllAddresses()
+    const changeAddress = await allAddress.next();
 
-    _tx.getShortHash();
-    
-    _tx.validate();
-    _tx.prepareToSend();
-    const getDataToSign =_tx.getDataToSign();
-    const toBytes = _tx.toBytes();
+    const options = {
+        outputs: txData.outputs,
+        // inputs: txData.inputs,
+        changeAddress: changeAddress.value.address
+    };
 
-    const createFromBytes = hathorLib.Transaction.createFromBytes(getDataToSign, network);
-    
-    console.log(createFromBytes);
-    
-    return createFromBytes.toHex();
+    const TransactionService = new hathorLib.SendTransactionWalletService(walletService, options)
+    // const prepare = await TransactionService.prepareTx()
 
+    TransactionService.run();
+
+
+    // const Transaction = new hathorLib.Transaction(TransactionService.inputs, TransactionService.outputs, TransactionService)
+
+    // Transaction.
+
+    console.log("sign", TransactionService)
 
 }
-
