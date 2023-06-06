451bf5308ea4811e18e3b9384778d15e1afee7d1
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2023-06-06 10:21:06 -0300

    update optional fee

--- src/features/nft/entity/index.js
+++ src/features/nft/entity/index.js
@@ -32,7 +32,7 @@
  * @property {string=} mintAuthorityAddress address responsible to mint more tokens (for hathor only)
  * @property {string=} meltAuthorityAddress address responsible to burn more tokens (for hathor only)
  * @property {string=} feeCurrency fee currency (for celo only)
- * @property {import("../../transaction/entity").Fee} fee fee
+ * @property {import("../../transaction/entity").Fee=} fee fee
  * @property {import('../../transaction/entity').SolanaCreator[]=} creators true if this token has fixed supply (for solana only)
  * @property {number=} royaltiesFee (for solana only)
  * @property {string=} collection (for solana only)
@@ -45,7 +45,7 @@
  * @property {string | number} amount amount to be transferred
  * @property {string=} tokenId token id to be transferred only for EVMs (ethereum, bsc, celo, polygon, avax, chiliz)
  * @property {string=} feeCurrency fee currency (for celo only)
- * @property {import("../../transaction/entity").Fee} fee fee
+ * @property {import("../../transaction/entity").Fee=} fee fee
 
  * 
  * @typedef {Object} NftMintInput
@@ -58,7 +58,7 @@
  * @property {string=} uri metadata URI string for EVMs this will be the base URI
  * @property {string=} mintAuthorityAddress mint authority address for hathor only
  * @property {string=} feeCurrency fee currency (for celo only)
- * @property {import("../../transaction/entity").Fee} fee fee
+ * @property {import("../../transaction/entity").Fee=} fee fee
  * 
  * @typedef {Object} NftBurnInput
  * @property {import('../../../services/blockchain/constants').Protocol} protocol
@@ -68,7 +68,7 @@
  * @property {string=} tokenId token id to be burnt only for EVMs (ethereum, bsc, celo, polygon, avax, chiliz)
  * @property {string=} meltAuthorityAddress melt authority address for hathor only
  * @property {string=} feeCurrency fee currency (for celo only)
- * @property {import("../../transaction/entity").Fee} fee fee
+ * @property {import("../../transaction/entity").Fee=} fee fee
 
  *
  * @typedef {Object} NftApproveInput
